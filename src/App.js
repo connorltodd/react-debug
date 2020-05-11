35bd253fa815b33c6694b9965649cb7bfb4a608f
DEV <connortodd0304@gmail.com>
2020-05-11 22:32:52 +0100

    live completed with the readme.md file

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Counter from './components/Counter';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: 0,
+    };
+  }
+
+  handleCounter = () => {
+    this.setState((prevState) => (
+      { counter: prevState.counter + 1 }
+    ));
+  }
+
+  render() {
+    const { counter } = this.state;
+    return (
+      <div className="App">
+        <h1>Counter</h1>
+        <Counter counter={counter} />
+        <button
+          type="button"
+          onClick={this.handleCounter}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          Increment Counter
+        </button>
+      </div>
+    );
+  }
 }
 
 export default App;
